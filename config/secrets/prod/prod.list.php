d7781dd3b188d28610a8eea6db62b0fbb99dbec5
Julián Bajo <programacion@piensoymascotas.com>
2026-07-02 16:20:53 +0200

    Agrega credenciales para Merchant a los secretos

--- config/secrets/prod/prod.list.php
+++ config/secrets/prod/prod.list.php
@@ -3,4 +3,8 @@
 return [
     'CONNECTIF_API_KEY' => null,
     'CRON_BEARER_TOKEN' => null,
+    'GOOGLE_SFTP_HOST' => null,
+    'GOOGLE_SFTP_PASSWORD' => null,
+    'GOOGLE_SFTP_PORT' => null,
+    'GOOGLE_SFTP_USER' => null,
 ];
