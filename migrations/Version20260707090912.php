74ea9c764359d32142172bf82d64618f031d2212
Julián Bajo <programacion@piensoymascotas.com>
2026-07-07 12:33:24 +0200

    Bug corregido en la migración, ')' de más

--- migrations/Version20260707090912.php
+++ migrations/Version20260707090912.php
@@ -22,7 +22,7 @@ final class Version20260707090912 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE priceshape_brand_banned (id_manufacturer INT NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY (id_manufacturer)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE priceshape_product_pvpr (id_product INT UNSIGNED NOT NULL, id_product_attribute INT UNSIGNED NOT NULL, country VARCHAR(2) NOT NULL, pvpr NUMERIC(6, 2) NOT NULL, PRIMARY KEY (id_product, id_product_attribute)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('CREATE TABLE priceshape_brand_fixed_price (id_manufacturer INT NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY (id_manufacturer)) DEFAULT CHARACTER SET utf8mb4)');
+        $this->addSql('CREATE TABLE priceshape_brand_fixed_price (id_manufacturer INT NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY (id_manufacturer)) DEFAULT CHARACTER SET utf8mb4');
     }
 
     public function down(Schema $schema): void
