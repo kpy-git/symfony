9fad08427e9f44dae23b3445400a7265af622560
Julián Bajo <bajoherrero.julian@pm.me>
2026-07-29 17:53:50 +0200

    Problema corregido en la migración, la tabla piceshape_brand_included no se creaba solo se modificaba

--- migrations/Version20260723150916.php
+++ migrations/Version20260723150916.php
@@ -21,13 +21,13 @@ final class Version20260723150916 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE kpy_product ADD weight DOUBLE PRECISION DEFAULT 0 NOT NULL');
-        $this->addSql('ALTER TABLE priceshape_brand_included ADD fixed_price TINYINT NOT NULL');
+        $this->addSql('CREATE TABLE priceshape_brand_included (id_manufacturer INT NOT NULL, created_at DATETIME NOT NULL, fixed_price TINYINT NOT NULL, PRIMARY KEY (id_manufacturer)) DEFAULT CHARACTER SET utf8mb4');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE kpy_product DROP weight');
-        $this->addSql('ALTER TABLE priceshape_brand_included DROP fixed_price');
+        $this->addSql('DROP TABLE priceshape_brand_included');
     }
 }
