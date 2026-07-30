99285b2f9ab17534d941de737a38cbbe5df6e759
Julián Bajo <bajoherrero.julian@pm.me>
2026-07-30 20:17:14 +0200

    Corrige la consulta que saca los productos que tienen stock sólo en Prestashop
    Ahora primero se basa en los productos que existen en product y product_attribute. Antes sacaba solo de stock_available y muchos -0 los ponia a 0 porque son productos con combinaciones que evidentemente el -0 no existe en Aqua

--- src/Warehouse/Query/StockPrestashopOnly.php
+++ src/Warehouse/Query/StockPrestashopOnly.php
@@ -18,11 +18,16 @@ readonly class StockPrestashopOnly implements QueryInterface
     public function fetch(array $params = []): array
     {
         return $this->kompyDatabase->execute(
-            "select sa.id_product, sa.id_product_attribute
-                from ps_stock_available sa
-                where sa.out_of_stock != 1
-                    and sa.quantity > 0
-                    and not exists (select 1 from ps_neftys_stock ns where ns.id_product=sa.id_product and ns.id_product_attribute=sa.id_product_attribute)"
+            "select p.id_product, ifnull(pa.id_product_attribute, 0) as id_product_attribute
+                    from ps_product p
+                    left join ps_product_attribute pa
+                        on pa.id_product = p.id_product
+                    left join ps_stock_available sa
+                        on sa.id_product = p.id_product and sa.id_product_attribute = ifnull(pa.id_product_attribute, 0)
+                    where sa.out_of_stock != 1
+                      and sa.quantity > 0
+                      and not exists (select 1 from ps_neftys_stock ns where ns.id_product=sa.id_product and ns.id_product_attribute=sa.id_product_attribute)
+                      and not exists (select 1 from ps_kpy_packs kp where kp.id_product_pack = CONCAT_WS('-', p.id_product, ifnull(pa.id_product_attribute, 0)))"
         );
     }
 }
