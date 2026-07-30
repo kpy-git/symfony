3937a65d8dd7d63843ab08738e5d93364d7a14cf
Julián Bajo <bajoherrero.julian@pm.me>
2026-07-30 16:28:17 +0200

    El stock de la tienda tiene en cuenta los pedidos pendientes de preparar

--- src/Warehouse/Query/StockKompyChinalesQuery.php
+++ src/Warehouse/Query/StockKompyChinalesQuery.php
@@ -18,9 +18,19 @@ readonly class StockKompyChinalesQuery implements QueryInterface
     public function fetch(array $params = []): array
     {
         return $this->aquaDatabase->execute(
-            "SELECT RTRIM(A.CODIGO) AS SKU, A.EXISTENCIA AS STOCK
+            "WITH PENDIENTES AS (
+                    SELECT MO.CODART, SUM(MO.UNIDADES - MO.INCORPORAD) AS UNIDADES
+                        FROM DATOP03 OP WITH(NOLOCK)
+                        INNER JOIN DATMO03 MO WITH(NOLOCK)
+                            ON MO.NUMERO = OP.NUMERO AND MO.UNIDADES > MO.INCORPORAD
+                        WHERE OP.TIPOOPER = 'C' AND OP.CENTRO = 'TIENDA' AND OP.PENDIENTES > 0
+                        GROUP BY MO.CODART
+                )
+                SELECT RTRIM(A.CODIGO) AS SKU, A.EXISTENCIA - ISNULL(PENDIENTES.UNIDADES, 0) AS STOCK
                 FROM DATAS03 A WITH(NOLOCK)
-                WHERE ALMACEN='TIENDA' AND A.EXISTENCIA > 0"
+                LEFT JOIN PENDIENTES ON PENDIENTES.CODART = A.CODIGO
+                WHERE A.ALMACEN = 'TIENDA'
+                    AND (A.EXISTENCIA - ISNULL(PENDIENTES.UNIDADES, 0)) > 0"
         );
     }
 }
